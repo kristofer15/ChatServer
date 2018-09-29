7e0dbe51072ca858df9774b0cd2f09f96b271334
Kristofer <kristofer15@ru.is>
2018-09-29 19:53:58 +0000

    Moved binaries to a directory

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
-g++ -std=c++17 server.cpp settings.cpp -o server
-g++ -std=c++17 client.cpp -o client
+mkdir -p bin
+g++ -std=c++17 server.cpp settings.cpp -o bin/server
+g++ -std=c++17 client.cpp -o bin/client
